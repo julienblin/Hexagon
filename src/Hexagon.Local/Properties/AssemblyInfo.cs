111c2db4df68fa357afd940f9e9c86efb884e595
Julien Blin <julien.blin@gmail.com>
2014-05-08 10:45:48 -0400

    Added Hexagon.Local empty project
    Inluding unit tests.

---
+++ src/Hexagon.Local/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Julien Blin">
+//   Copyright (c) 2014 Julien Blin
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Hexagon.Local")]
+[assembly: AssemblyDescription("Local message processor implementation for Hexagon.")]
---
+++ src/Tests/Hexagon.Local.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Julien Blin">
+//   Copyright (c) 2014 Julien Blin
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Hexagon.Local.Tests")]
+[assembly: AssemblyDescription("Unit tests for Hexagon.Local")]
