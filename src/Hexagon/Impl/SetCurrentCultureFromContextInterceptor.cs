43d9ebdfe8546538bbb92b332b92ed2664b5b9f7
Julien Blin <julien.blin@gmail.com>
2014-05-21 16:36:36 -0400

    Added interceptors to handle culture continuity in disconnected scenarios.

---
+++ src/Hexagon/Impl/SetCurrentCultureFromContextInterceptor.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SetCurrentCultureFromContextInterceptor.cs" company="Julien Blin">
+//   Copyright (c) 2014 Julien Blin
+// </copyright>
+// <summary>
+//   Sets the <see cref="Thread.CurrentCulture" /> and <see cref="Thread.CurrentUICulture" /> properties
+//   using the values stored in <see cref="SetCurrentCultureInContextInterceptor.CultureHeaderKey" />
+//   and <see cref="SetCurrentCultureInContextInterceptor.UICultureHeaderKey" />.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Hexagon.Impl
+{
+    using System.Globalization;
+    using System.Threading;
+
+    /// <summary>
+    /// Sets the <see cref="Thread.CurrentCulture"/> and <see cref="Thread.CurrentUICulture"/> properties
+    /// using the values stored in <see cref="SetCurrentCultureInContextInterceptor.CultureHeaderKey"/>
+    /// and <see cref="SetCurrentCultureInContextInterceptor.UICultureHeaderKey"/>, if any.
+    /// </summary>
+    /// <remarks>
+    /// See <see cref="SetCurrentCultureInContextInterceptor"/> for the reverse operation.
+    /// </remarks>
+    public class SetCurrentCultureFromContextInterceptor: IRequestProcessorInterceptor
+    {
+        /// <summary>
+        /// Gets the interception priority.
+        /// Defined as <see cref="DefaultInterceptionPrority.High"/>.
+        /// </summary>
+        public int InterceptionPriority
+        {
+            get
+            {
+                return DefaultInterceptionPrority.High;
+            }
+        }
+
+        /// <inheritdoc />
+        public void Intercept(IRequestProcessorInvocation invocation)
+        {
+            if (invocation.Request.Context.Headers.ContainsKey(SetCurrentCultureInContextInterceptor.CultureHeaderKey))
+            {
+                Thread.CurrentThread.CurrentCulture =
+                    CultureInfo.GetCultureInfo(
+                        invocation.Request.Context.Headers[SetCurrentCultureInContextInterceptor.CultureHeaderKey]);
+            }
+
+            if (invocation.Request.Context.Headers.ContainsKey(SetCurrentCultureInContextInterceptor.UICultureHeaderKey))
+            {
+                Thread.CurrentThread.CurrentUICulture =
+                    CultureInfo.GetCultureInfo(
+                        invocation.Request.Context.Headers[SetCurrentCultureInContextInterceptor.UICultureHeaderKey]);
+            }
+
+            invocation.Proceed();
+        }
+    }
+}
---
+++ src/Hexagon/Impl/SetCurrentCultureInContextInterceptor.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SetCurrentCultureInContextInterceptor.cs" company="Julien Blin">
+//   Copyright (c) 2014 Julien Blin
+// </copyright>
+// <summary>
+//   Gets the <see cref="CultureInfo.CurrentCulture"/> and <see cref="CultureInfo.CurrentCulture"/>
+//   <see cref="CultureInfo.Name"/> and puts it into the <see cref="IMessage.Context"/> <see cref="IContext.Headers"/>
+//   using the key <see cref="ContextHeaderKey"/>.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Hexagon.Impl
+{
+    using System.Globalization;
+
+    using Hexagon.Messages;
+
+    /// <summary>
+    /// Gets the <see cref="CultureInfo.CurrentCulture"/> and <see cref="CultureInfo.CurrentUICulture"/>
+    /// <see cref="CultureInfo.Name"/> and puts it into the <see cref="IMessage.Context"/> <see cref="IContext.Headers"/>
+    /// using the <see cref="CultureHeaderKey"/> and <see cref="UICultureHeaderKey"/> keys.
+    /// </summary>
+    /// <remarks>
+    /// See <see cref="SetCurrentCultureFromContextInterceptor"/> for the reverse operation.
+    /// </remarks>
+    public class SetCurrentCultureInContextInterceptor : IRequestProcessorInterceptor
+    {
+        /// <summary>
+        /// The <see cref="IContext.Headers"/> key used for <see cref="CultureInfo.CurrentCulture"/>.
+        /// </summary>
+        public const string CultureHeaderKey = @"Culture";
+
+        /// <summary>
+        /// The <see cref="IContext.Headers"/> key used for <see cref="CultureInfo.CurrentUICulture"/>.
+        /// </summary>
+        public const string UICultureHeaderKey = @"UICulture";
+
+        /// <summary>
+        /// Gets the interception priority.
+        /// Defined as <see cref="DefaultInterceptionPrority.Low"/>.
+        /// </summary>
+        public int InterceptionPriority
+        {
+            get
+            {
+                return DefaultInterceptionPrority.Low;
+            }
+        }
+
+        /// <inheritdoc />
+        public void Intercept(IRequestProcessorInvocation invocation)
+        {
+            invocation.Request.Context.Headers[CultureHeaderKey] = CultureInfo.CurrentCulture.Name;
+            invocation.Request.Context.Headers[UICultureHeaderKey] = CultureInfo.CurrentUICulture.Name;
+            invocation.Proceed();
+        }
+    }
+}
